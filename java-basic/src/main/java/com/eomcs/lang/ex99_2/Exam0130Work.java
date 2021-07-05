b609d81d72a9047cfb5a84d48e81e04b85f3c359
SeonyoungHa <syh5004@naver.com>
2021-07-05 20:49:05 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex99_2/Exam0130Work.java
+++ java-basic/src/main/java/com/eomcs/lang/ex99_2/Exam0130Work.java
@@ -3,8 +3,7 @@ package com.eomcs.lang.ex99_2;
 public class Exam0130Work {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    System.out.println();
   }
 
 }
