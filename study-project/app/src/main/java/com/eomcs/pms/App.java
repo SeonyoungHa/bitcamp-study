98726e81d775d33a93e5c987d1d4c71dedf449ad
SeonyoungHa <syh5004@naver.com>
2021-07-07 18:03:32 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -2,7 +2,8 @@ package com.eomcs.pms;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("[회원]");
+
+    /*System.out.println("[회원]");
     System.out.println("번호: 101");
     System.out.println("이름: 홍길동");
     System.out.println("이메일: hong@test.com");
@@ -10,5 +11,45 @@ public class App {
     System.out.println("사진: hong.jpg");
     System.out.println("전화: 010-1111-2222");
     System.out.println("가입일: 2021-07-05");
+     */
+
+    java.util.Scanner keyboardScanner = new java.util.Scanner(System.in);
+
+    System.out.println("[회원]");
+    System.out.print("번호? ");
+    int no = keyboardScanner.nextInt();
+    keyboardScanner.nextLine();//키보드 메모리에 남아있는 잉여
+
+    System.out.print("이름? ");
+    String name = keyboardScanner.nextLine();
+
+    System.out.print("이메일? ");
+    String email = keyboardScanner.nextLine();
+
+    System.out.print("암호? ");
+    String password = keyboardScanner.nextLine();
+
+    System.out.print("사진? ");
+    String photo = keyboardScanner.nextLine();
+
+    System.out.print("전화? ");
+    String tel = keyboardScanner.nextLine();
+
+    /*System.out.print("가입일? ");
+    String registerDate = keyboardScanner.nextLine();
+     */
+    java.sql.Date registerDate= new java.sql.Date(System.currentTimeMillis());
+
+    keyboardScanner.close();
+
+    System.out.println("----------------------");
+    System.out.printf("번호: %s\n", no);
+    System.out.printf("이름: %s\n", name);
+    System.out.printf("이메일: %s\n", email);
+    System.out.printf("암호: %s\n", password);
+    System.out.printf("사진: %s\n", photo);
+    System.out.printf("전화: %s\n", tel);
+    System.out.printf("가입일: %s\n", registerDate);
+
   }
 }
