db30b435d39c551f99f031c16e1e8137dc2f39d8
SeonyoungHa <syh5004@naver.com>
2021-07-08 15:16:16 +0900

    .

--- java-basic/src/main/java/com/eomcs/basic/ex10/Exam0110.java
+++ java-basic/src/main/java/com/eomcs/basic/ex10/Exam0110.java
@@ -2,10 +2,9 @@ package com.eomcs.basic.ex10;
 
 public class Exam0110 {
   public static void main(String[] args) {
-    long millis = System.currentTimeMillis();
-    System.out.println(millis);
 
-    java.sql.Date d = new java.sql.Date(millis);
-    System.out.print(d.toString());
+    java.util.Date d = new java.util.Date(); // Date 객체를 만드는 순간의 시간을 저장해 둔다.
+    System.out.printf("%tY-%tm-%td\n", d, d, d);
+    System.out.printf("%tY-%1$tm-%1$td\n", d);
   }
 }
