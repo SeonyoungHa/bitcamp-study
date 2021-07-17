4df1b228ef5aa446b21f2c384eff29cf386a2bd6
SeonyoungHa <syh5004@naver.com>
2021-07-17 18:06:52 +0900

    .

---
+++ study-project/app/src/main/java/com/nam/Study.java
@@ -0,0 +1,5 @@
+package com.nam;
+
+public class Study {
+
+}
---
+++ study-project/app/src/main/java/com/nam/ch03/Excercise3_4.java
@@ -0,0 +1,9 @@
+package com.nam.ch03;
+
+public class Excercise3_4 {
+
+  public static void main(String[] args) {
+    int num = 456;
+    System.out.println(num/100*100);
+  }
+}
