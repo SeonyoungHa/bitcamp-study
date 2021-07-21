c16c3a2cc26d41ed3e236bd77231a1d9aa645e9b
SeonyoungHa <syh5004@naver.com>
2021-07-21 15:31:59 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex00/Test.java
+++ java-basic/src/main/java/com/eomcs/oop/ex00/Test.java
@@ -1,10 +1,14 @@
 package com.eomcs.oop.ex00;
 
 public class Test {
-	static int a;
-	int b;
-	
-	void m(int c) {
-		int d = c + 100;
-	}
+  static int a;
+  int b;
+
+  static void m1(int c) {
+    int d = c + 100;
+  }
+
+  void m2(int c) {
+    int d = c + 100;
+  }
 }
