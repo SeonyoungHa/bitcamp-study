59c434966663c984d0f1e04861635280a3625121
SeonyoungHa <syh5004@naver.com>
2021-07-29 14:41:54 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex06/a/Exam0410.java
+++ java-basic/src/main/java/com/eomcs/oop/ex06/a/Exam0410.java
@@ -6,8 +6,8 @@ public class Exam0410 {
   // Sedan과 Truck의 모델명과 cc를 출력하라!
 
   public static void printSedan(Sedan sedan) {
-    System.out.printf("모델명: %s\n", sedan.model);
-    System.out.printf("cc: %d\n", sedan.cc);
+    System.out.printf("모델명: %s\n", sedan.model); // Vehicle의 설계도로 만든 인스턴스 변수
+    System.out.printf("cc: %d\n", sedan.cc); // Car 설계도로 만든 인스턴스 변수
     System.out.println("-------------------------");
   }
 
@@ -23,7 +23,7 @@ public class Exam0410 {
     printSedan(sedan);
 
     // printSedan()의 파라미터는 Sedan 객체의 주소만 받을 수 있다.
-    // 그래서 Truck 객체를 전달할 수 없다.
+    // 그래서 Truck 객체를 전달할 수 없다. (Truck은 Sedan의 자식이 아님)
     //    printSedan(truck); // 컴파일 오류!
 
     // Truck 인스턴스에서 model과 cc 값을 꺼내서 출력할 메서드를
