75901a857cda414d676a44be1ba96838ca45c6d1
SeonyoungHa <syh5004@naver.com>
2021-07-30 11:12:03 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex06/c/Exam0421.java
+++ java-basic/src/main/java/com/eomcs/oop/ex06/c/Exam0421.java
@@ -9,57 +9,52 @@ package com.eomcs.oop.ex06.c;
 //
 
 public class Exam0421 {
-  static class X {
-    void m1() {
-      System.out.println("X의 m1()");
-    }
-    void m2() {
-      System.out.println("X의 m2()");
-    }
-  }
+	static class X {
+		void m1() { System.out.println("X의 m1()"); }
+		void m2() { System.out.println("X의 m2()"); }
+	}
 
 
-  static class X2 extends X {
-    @Override
-    void m1() {
-      System.out.println("X2의 m1()");
-    }
-  }
+	static class X2 extends X {
+		@Override
+		void m1() { System.out.println("X2의 m1()"); }
+	}
 
 
-  static class X3 extends X2 {
-    @Override
-    void m2() {
-      System.out.println("X3의 m2()");
-    }
-  }
+	static class X3 extends X2 {
+		@Override
+		void m2() { System.out.println("X3의 m2()"); }
+	}
 
 
-  static class X4 extends X3 {
-    @Override
-    void m1() {
-      System.out.println("X4의 m1()");
-    }
+	static class X4 extends X3 {
+		@Override
+		void m1() { System.out.println("X4의 m1()"); }
 
-    void test() {
-      this.m1(); // X4의 m1()
-      super.m1(); // X2의 m1()
+		void test() {
+			this.m1(); // X4의 m1()
+			super.m1(); // X2의 m1()
 
-      this.m2(); // X3의 m2()
-      super.m2(); // X3의 m2()
+			this.m2(); // X3의 m2()
+			super.m2(); // X3의 m2()
 
-      //      super.super.m1(); // 컴파일 오류! 이런 문법은 없다! 무협지 문법!
+			//      super.super.m1(); // 컴파일 오류! 이런 문법은 없다! 무협지 문법!
 
-    }
-  }
+		}
+	}
 
-  public static void main(String[] args) {
-    X4 obj = new X4();
-    obj.test();
+	static class X5 extends X4{
+		@Override void m1() { System.out.println("X5의 m1()"); }
+		@Override void m2() { System.out.println("X5의 m2()"); }
+	}
 
-    // 레퍼런스에서 super를 사용할 수 없다.
-    //    obj.super.m1(); // 컴파일 오류!
-  }
+	public static void main(String[] args) {
+		X5 obj = new X5();
+		obj.test();
+
+		// 레퍼런스에서 super를 사용할 수 없다.
+		//    obj.super.m1(); // 컴파일 오류!
+	}
 }
 
 
