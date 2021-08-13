cbda849adc030dd412baa424426011e9d267449a
SeonyoungHa <syh5004@naver.com>
2021-08-13 15:25:55 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex11/c/Exam0210.java
+++ java-basic/src/main/java/com/eomcs/oop/ex11/c/Exam0210.java
@@ -6,15 +6,7 @@ class B {
   static int v1 = 10;
   static void m1() {}
 
-  // 인스턴스 멤버
-  int v2 = 20;
-  int v3 = 30;
-  int v4 = 40;
-  void m2() {}
-
   class X {
-    int v3 = 300;
-
     void test() {
       // 같은 클래스 멤버 접근 가능
       System.out.println(B.v1);
@@ -23,26 +15,8 @@ class B {
 
       // 같은 클래스 멤버이기 때문에 바깥 클래스 이름 생략 가능
       System.out.println(v1); 
-      m2(); 
+      m1(); 
       System.out.println("-------------------------");
-
-
-      int v2 = 2000;
-
-      // 인스턴스 멤버에 접근 가능
-      // =>   바깥클래스명.this.인스턴스멤버명
-      System.out.println(B.this.v2); 
-      System.out.println(B.this.v3); 
-      System.out.println(B.this.v4); 
-      System.out.println("-------------------------");
-
-      // this 를 생략하면 항상 가장 가까운 곳부터 찾는다.
-      System.out.println(v2); // => test() 메서드의 로컬 변수를 가리킨다.
-      System.out.println(v3); // => this.v3 : X 클래스의 인스턴스 변수를 가리킨다.
-      System.out.println(v4); // => B.this.v4 : 바깥 클래스 B의 인스턴스 변수를 가리킨다.
-
-      // => inner class 의 가장 큰 특징은 바깥 클래스의 다른 멤버에 손쉽게 접근할 수 있다는 것이다.
-      // => 이것이 가능한 이유는 바깥 클래스의 인스턴스 멤버이기 때문이다.
     }
   }
 }
