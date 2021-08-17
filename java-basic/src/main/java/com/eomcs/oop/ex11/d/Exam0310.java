d4d9bc89b4bd88f5bf8086e4f5fd7b1bf3ea474c
SeonyoungHa <syh5004@naver.com>
2021-08-17 15:04:23 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex11/d/Exam0310.java
+++ java-basic/src/main/java/com/eomcs/oop/ex11/d/Exam0310.java
@@ -30,8 +30,8 @@ class D {
 			//        private final synthetic int val$v2; <== 바깥 메서드의 로컬 변수 저장
 			//
 			void f1() {
-				// 그래서 로컬 클래스에서는 바깥 메서드의 로컬 변수를
-				// 마치 자기것인양 사용할 수 있는 것이다.
+				// 그래서 다음과 같이
+				// 로컬 클래스에서는 바깥 메서드의 로컬 변수를 자기것인양 사용할 수 있는 것이다.
 				System.out.println(a); // m1() 에 선언된 로컬 변수를 가르킨다.
 			}
 		}
@@ -40,22 +40,18 @@ class D {
 	}
 
 	static void m2() {
-		int a;
-		int b;
+		int a = 100;
+		int b = 200;
 
+		// 스태틱 메서드의 로컬 클래스도 인스턴스 메서드의 로컬 클래스와 같다.
 		class X {
-
-			// 로컬 클래스는 바깥 메서드의 로컬 변수의 값을 저장할 필드를 자동으로 추가한다.
-			// .class 파일의 코드:
-			//      class com.eomcs.oop.ex11.d.D$1X {
-			//
-			//        int v3; <== 로컬 클래스의 인스턴스 필드
-			//        final synthetic com.eomcs.oop.ex11.d.D this$0;  <== 바깥 클래스의 인스턴스 주소 저장
-			//        private final synthetic int val$v2; <== 바깥 메서드의 로컬 변수 저장
-			//
+			void f1() {
+				System.out.println(b); // m1() 에 선언된 로컬 변수를 가르킨다.
+			}
 		}
 
 		X obj = new X();
+		obj.f1();
 	}
 }
 
--- java-basic/src/main/java/com/eomcs/oop/ex11/d/Exam0321.java
+++ java-basic/src/main/java/com/eomcs/oop/ex11/d/Exam0321.java
@@ -1,7 +1,7 @@
-// local class 에서 바깥 메서드의 로컬 변수 접근
+// local class 에서 바깥 메서드의 로컬 변수 접근 I
 package com.eomcs.oop.ex11.d;
 
-class D {
+class D2 {
 	int v1 = 1;
 
 	void m1() {
@@ -10,22 +10,14 @@ class D {
 		class X {
 			int v3 = 3;
 
-			// 로컬 클래스는 바깥 클래스의 인스턴스 주소를 저장할 필드가 있을 뿐만 아니라
-			// 인스턴스 메서드 안에 선언된 로컬 변수의 값을 저장할 필드도 있다.
-			// .class 파일의 코드:
-			//      class com.eomcs.oop.ex11.d.D$1X {
-			//
-			//        int v3; <== 로컬 클래스의 인스턴스 필드
-			//        final synthetic com.eomcs.oop.ex11.d.D this$0;  <== 바깥 클래스의 인스턴스 주소 저장
-			//        private final synthetic int val$v2; <== 바깥 메서드의 로컬 변수 저장
-			//
-
 			void f() {
 				int v4 = 4;
-				//				System.out.printf("v4 = %d\n", v4);
-				//				System.out.printf("v3 = %d\n", this.v3);
-				//				System.out.printf("v2 = %d\n", v2);
-				//				System.out.printf("v1 = %d\n", D.this.v1);
+				System.out.printf("v4 = %d\n", v4); 
+				System.out.printf("v3 = %d\n", this.v3);
+				System.out.printf("v2 = %d\n", v2);
+				System.out.printf("v1 = %d\n", D2.this.v1);
+
+				// 바깥 메서드의 로컬 변수를 사용할 때는 
 			}
 		}
 
@@ -37,7 +29,7 @@ class D {
 public class Exam0321 {
 
 	public static void main(String[] args) {
-		D obj = new D();
+		D2 obj = new D2();
 		obj.m1();
 	}
 
