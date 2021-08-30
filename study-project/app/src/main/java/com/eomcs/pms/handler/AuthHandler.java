81fd007e09fffe30ea3c8ad02e7fb49695aa6180
SeonyoungHa <syh5004@naver.com>
2021-08-30 17:41:27 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/handler/AuthHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/AuthHandler.java
@@ -7,6 +7,13 @@ import com.eomcs.util.Prompt;
 public class AuthHandler {
 
   List<Member> memberList;
+  static Member loginUser; //로그인 정보를 담을 필드 추가
+  // displayLoginUser() 메서드 추가
+
+  public static Member getLoginUser() {
+    return loginUser; //   // login()변경: 로그인 한 후 멤버 객체를 로그인 필드에 저장
+
+  }
 
   public AuthHandler(List<Member> memberList) {
     this.memberList = memberList;
@@ -20,17 +27,35 @@ public class AuthHandler {
     Member member = findByEmailPassword(email, password);
 
     if (member == null) {
-      System.out.println("해당 번호의 회원이 없습니다.");
-      return;
+      System.out.println("이메일과 암호가 일치하는 회원을 찾을 수 없습니다.");
+    } else {
+      System.out.printf("%s님 환영합니다!\n", member.getName());
     }
-
-    System.out.printf("이름: %s\n", member.getName());
-    System.out.printf("이메일: %s\n", member.getEmail());
-    System.out.printf("사진: %s\n", member.getPhoto());
-    System.out.printf("전화: %s\n", member.getTel());
-    System.out.printf("등록일: %s\n", member.getRegisteredDate());
   }
 
+  public void displayLoginUser() {
+    System.out.println("[내정보]");
+
+    if (loginUser == null) {
+      System.out.println("로그인 하지 않았습니다.");
+      return;
+    } 
+
+    System.out.printf("이름: %s\n", loginUser.getName());
+    System.out.printf("이메일: %s\n", loginUser.getEmail());
+    System.out.printf("사진: %s\n", loginUser.getPhoto());
+    System.out.printf("전화: %s\n", loginUser.getTel());
+    System.out.printf("등록일: %s\n", loginUser.getRegisteredDate());
+  }
+
+
+
+  public void logout() {
+    System.out.println("[로그아웃]");
+    loginUser = null;
+    System.out.println("로그인 하지 않았습니다.");
+    return;
+  }
 
   private Member findByEmailPassword(String email, String password) {
     Member[] arr = memberList.toArray(new Member[0]);
@@ -43,7 +68,9 @@ public class AuthHandler {
     return null;
   }
 
+
 }
+
 
 
 
