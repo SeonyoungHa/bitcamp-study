c9b7928b3d9853b39cbde7927895444023d4cbcc
SeonyoungHa <syh5004@naver.com>
2021-09-01 11:15:02 +0900

    upload-command-pattern

--- study-project/app/src/main/java/com/eomcs/pms/handler/BoardUpdateHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/BoardUpdateHandler.java
@@ -1,6 +1,5 @@
 package com.eomcs.pms.handler;
 
-import java.sql.Date;
 import java.util.List;
 import com.eomcs.pms.domain.Board;
 import com.eomcs.util.Prompt;
@@ -13,54 +12,6 @@ public class BoardUpdateHandler {
     this.boardList = boardList;
   }
 
-  public void add() {
-    System.out.println("[새 게시글]");
-
-    Board board = new Board();
-
-    board.setNo(Prompt.inputInt("번호? "));
-    board.setTitle(Prompt.inputString("제목? "));
-    board.setContent(Prompt.inputString("내용? "));
-
-    board.setWriter(AuthHandler.getLoginUser());
-    board.setRegisteredDate(new Date(System.currentTimeMillis()));
-
-    boardList.add(board);
-  }
-
-  public void list() {
-    System.out.println("[게시글 목록]");
-
-    for (Board board : boardList) {
-      System.out.printf("%d, %s, %s, %s, %d, %d\n", 
-          board.getNo(), 
-          board.getTitle(), 
-          board.getWriter().getName(),
-          board.getRegisteredDate(),
-          board.getViewCount(), 
-          board.getLike());
-    }
-  }
-
-  public void detail() {
-    System.out.println("[게시글 상세보기]");
-    int no = Prompt.inputInt("번호? ");
-
-    Board board = findByNo(no);
-
-    if (board == null) {
-      System.out.println("해당 번호의 게시글이 없습니다.");
-      return;
-    }
-
-    System.out.printf("제목: %s\n", board.getTitle());
-    System.out.printf("내용: %s\n", board.getContent());
-    System.out.printf("작성자: %s\n", board.getWriter().getName());
-    System.out.printf("등록일: %s\n", board.getRegisteredDate());
-
-    board.setViewCount(board.getViewCount() + 1);
-    System.out.printf("조회수: %d\n", board.getViewCount());
-  }
 
   public void update() {
     System.out.println("[게시글 변경]");
@@ -92,38 +43,11 @@ public class BoardUpdateHandler {
     System.out.println("게시글을 변경하였습니다.");
   }
 
-  public void delete() {
-    System.out.println("[게시글 삭제]");
-    int no = Prompt.inputInt("번호? ");
-
-    Board board = findByNo(no);
-
-    if (board == null) {
-      System.out.println("해당 번호의 게시글이 없습니다.");
-      return;
-    }
-
-    if (board.getWriter().getNo() != AuthHandler.getLoginUser().getNo()) {
-      System.out.println("삭제 권한이 없습니다.");
-      return;
-    }
-
-    String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
-    if (input.equalsIgnoreCase("n") || input.length() == 0) {
-      System.out.println("게시글 삭제를 취소하였습니다.");
-      return;
-    }
-
-    boardList.remove(board);
-
-    System.out.println("게시글을 삭제하였습니다.");
-  }
-
   private Board findByNo(int no) {
     // 일부러 BoardList에 들어 있는 배열 보다 작은 배열을 넘겨준다.
     // => 그러면 toArray()는 새 배열을 만들어 값을 저장한 후 리턴할 것이다.
     Board[] arr = boardList.toArray(new Board[0]);
-    for (Board board : arr) {
+    for (Board board : boardList) {
       if (board.getNo() == no) {
         return board;
       }
