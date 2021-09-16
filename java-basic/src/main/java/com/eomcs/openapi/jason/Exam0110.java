c133b70f488e4b46dce9f1e6fafc354c9f4ba16b
SeonyoungHa <syh5004@naver.com>
2021-09-16 11:20:49 +0900

    Upload openapi package

---
+++ java-basic/src/main/java/com/eomcs/openapi/jason/Exam0110.java
@@ -0,0 +1,25 @@
+// 객체 --> HSON 문자열 : 객체의 필드 값을 json 형식의 문자열로 만들기
+package com.eomcs.openapi.jason;
+
+import com.google.gson.Gson;
+
+public class Exam0110 {
+	public static void main(String[] args) {
+
+		// 1) 객체 준비
+		Member m = new Member();
+		m.setNo(100);
+		m.setName("홍길동");
+		m.setEmail("hong@test.com");
+		m.setPassword("1111");
+		m.setPhoto("hong.gif");
+		m.setTel("010-2222-1111");
+		m.setRegisteredDate(new Date(System.currentTimeMillis()));
+
+
+		Gson gson = new Gson();
+		String jsonStr = gson.toJson(null)
+
+				System.out.println(jsonStr);
+	}
+}
