b1a333fd65f99ff064d8b8ab6e22fe758bf6c099
SeonyoungHa <syh5004@naver.com>
2021-09-16 14:26:21 +0900

    Create Project,Task

--- java-basic/src/main/java/com/eomcs/openapi/json/Exam0420.java
+++ java-basic/src/main/java/com/eomcs/openapi/json/Exam0420.java
@@ -9,13 +9,9 @@ public class Exam0420 {
 		// 1) JSON 문자열 준비
 		String jsonStr = "{\"no\":1,\"title\":\"제목\",\"content\":\"내용\",\"writer\":{\"no\":100,\"name\":\"홍길동\",\"email\":\"hong@test.com\",\"password\":\"1111\",\"photo\":\"hong.gif\",\"tel\":\"010-2222-1111\",\"registeredDate\":\"9월 16, 2021\"},\"registeredDate\":\"9월 16, 2021\",\"viewCount\":98,\"like\":5}\n";
 
-		// 2) JSON 처리 객체 준비
-		Gson gson = new Gson();
+		Board b = new Gson().fromJson(jsonStr, Board.class);
 
-		// 3) JSON 문자열을 가지고 객체 만들기
-		Member m = gson.fromJson(jsonStr, Member.class);
-
-		System.out.println(m);
+		System.out.println(b);
 	}
 }
 
