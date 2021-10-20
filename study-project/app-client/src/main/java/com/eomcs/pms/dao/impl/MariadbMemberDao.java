e173c50a1fdb2c91e87f75964c75bb8c6296388c
SeonyoungHa <syh5004@naver.com>
2021-10-20 10:25:25 +0900

    Setting mybatis package

--- study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MariadbMemberDao.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MariadbMemberDao.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
 
@@ -14,132 +13,156 @@ import com.eomcs.pms.domain.Member;
 //
 public class MariadbMemberDao implements MemberDao {
 
-	Connection con;
+  Connection con;
 
-	public MariadbMemberDao(Connection con) {
-		this.con = con;
-	}
+  public MariadbMemberDao(Connection con) {
+    this.con = con;
+  }
 
-	@Override
-	public void insert(Member member) throws Exception {
-		try (PreparedStatement stmt = con.prepareStatement(
-				"insert into pms_member(name,email,password,photo,tel) values(?,?,password(?),?,?)")) {
+  @Override
+  public void insert(Member member) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "insert into pms_member(name,email,password,photo,tel) values(?,?,password(?),?,?)")) {
 
-			stmt.setString(1, member.getName());
-			stmt.setString(2, member.getEmail());
-			stmt.setString(3, member.getPassword());
-			stmt.setString(4, member.getPhoto());
-			stmt.setString(5, member.getTel());
+      stmt.setString(1, member.getName());
+      stmt.setString(2, member.getEmail());
+      stmt.setString(3, member.getPassword());
+      stmt.setString(4, member.getPhoto());
+      stmt.setString(5, member.getTel());
 
-			if (stmt.executeUpdate() == 0) {
-				throw new Exception("회원 데이터 저장 실패!");
-			}
-		}
-	}
+      if (stmt.executeUpdate() == 0) {
+        throw new Exception("회원 데이터 저장 실패!");
+      }
+    }
+  }
 
-	@Override
-	public List<Member> findAll() throws Exception {
-		try (PreparedStatement stmt = con.prepareStatement(
-				"select member_no,name,email,tel,created_dt from pms_member order by name asc");
-				ResultSet rs = stmt.executeQuery()) {
+  @Override
+  public List<Member> findAll() throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select member_no,name,email,tel,created_dt from pms_member order by name asc");
+        ResultSet rs = stmt.executeQuery()) {
 
-			ArrayList<Member> list = new ArrayList<>();
+      ArrayList<Member> list = new ArrayList<>();
 
-			while (rs.next()) {
-				Member member = new Member();
-				member.setNo(rs.getInt("member_no"));
-				member.setName(rs.getString("name"));
-				member.setEmail(rs.getString("email"));
-				member.setTel(rs.getString("tel"));
-				member.setRegisteredDate(rs.getDate("created_dt"));
+      while (rs.next()) {
+        Member member = new Member();
+        member.setNo(rs.getInt("member_no"));
+        member.setName(rs.getString("name"));
+        member.setEmail(rs.getString("email"));
+        member.setTel(rs.getString("tel"));
+        member.setRegisteredDate(rs.getDate("created_dt"));
 
-				list.add(member);
-			}
+        list.add(member);
+      }
 
-			return list;
-		}
-	}
+      return list;
+    }
+  }
 
-	@Override
-	public Member findByNo(int no) throws Exception {
-		try (PreparedStatement stmt = con.prepareStatement(
-				"select member_no,name,email,photo,tel,created_dt from pms_member where member_no=" + no);
-				ResultSet rs = stmt.executeQuery()) {
+  @Override
+  public Member findByNo(int no) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select member_no,name,email,photo,tel,created_dt from pms_member where member_no=" + no);
+        ResultSet rs = stmt.executeQuery()) {
 
-			if (!rs.next()) {
-				return null;
-			}
+      if (!rs.next()) {
+        return null;
+      }
 
-			Member member = new Member();
-			member.setNo(rs.getInt("member_no"));
-			member.setName(rs.getString("name"));
-			member.setEmail(rs.getString("email"));
-			member.setPhoto(rs.getString("photo"));
-			member.setTel(rs.getString("tel"));
-			member.setRegisteredDate(rs.getDate("created_dt"));
+      Member member = new Member();
+      member.setNo(rs.getInt("member_no"));
+      member.setName(rs.getString("name"));
+      member.setEmail(rs.getString("email"));
+      member.setPhoto(rs.getString("photo"));
+      member.setTel(rs.getString("tel"));
+      member.setRegisteredDate(rs.getDate("created_dt"));
 
-			return member;
-		}
-	}
+      return member;
+    }
+  }
 
-	@Override
-	public Member findByName(String name) throws Exception {
-		try (PreparedStatement stmt = con.prepareStatement(
-				"select member_no,name,email,photo,tel,created_dt from pms_member "
-						+ "where member_no=?" )) {
+  @Override
+  public Member findByName(String name) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select member_no,name,email,photo,tel,created_dt from pms_member"
+            + " where name=?")) {
 
-			stmt.setString(1, name);
+      stmt.setString(1, name);
 
-			try (ResultSet rs = stmt.executeQuery()) {
-				if (!rs.next()) {
-					return null;
-				}
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (!rs.next()) {
+          return null;
+        }
+        Member member = new Member();
+        member.setNo(rs.getInt("member_no"));
+        member.setName(rs.getString("name"));
+        member.setEmail(rs.getString("email"));
+        member.setPhoto(rs.getString("photo"));
+        member.setTel(rs.getString("tel"));
+        member.setRegisteredDate(rs.getDate("created_dt"));
+        return member;
+      }
+    }
+  }
 
-				Member member = new Member();
-				member.setNo(rs.getInt("member_no"));
-				member.setName(rs.getString("name"));
-				member.setEmail(rs.getString("email"));
-				member.setPhoto(rs.getString("photo"));
-				member.setTel(rs.getString("tel"));
-				member.setRegisteredDate(rs.getDate("created_dt"));
+  @Override
+  public Member findByEmailAndPassword(String email, String password) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select member_no,name,email,photo,tel,created_dt from pms_member"
+            + " where email=? and password=password(?)")) {
 
-				return member;
-			}
-		}
-	}
+      stmt.setString(1, email);
+      stmt.setString(2, password);
 
-	@Override
-	public void update(Member member) throws Exception {
-		try (PreparedStatement stmt = con.prepareStatement(
-				"update pms_member set"
-						+ " name=?,email=?,password=password(?),photo=?,tel=?"
-						+ " where member_no=?")) {
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (!rs.next()) {
+          return null;
+        }
+        Member member = new Member();
+        member.setNo(rs.getInt("member_no"));
+        member.setName(rs.getString("name"));
+        member.setEmail(rs.getString("email"));
+        member.setPhoto(rs.getString("photo"));
+        member.setTel(rs.getString("tel"));
+        member.setRegisteredDate(rs.getDate("created_dt"));
+        return member;
+      }
+    }
+  }
 
-			stmt.setString(1, member.getName());
-			stmt.setString(2, member.getEmail());
-			stmt.setString(3, member.getPassword());
-			stmt.setString(4, member.getPhoto());
-			stmt.setString(5, member.getTel());
-			stmt.setInt(6, member.getNo());
 
-			if (stmt.executeUpdate() == 0) {
-				throw new Exception("회원 데이터 변경 실패!");
-			}
-		}
-	}
+  @Override
+  public void update(Member member) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "update pms_member set"
+            + " name=?,email=?,password=password(?),photo=?,tel=?"
+            + " where member_no=?")) {
 
-	@Override
-	public void delete(int no) throws Exception {
-		try (PreparedStatement stmt = con.prepareStatement(
-				"delete from pms_member where member_no=?")) {
+      stmt.setString(1, member.getName());
+      stmt.setString(2, member.getEmail());
+      stmt.setString(3, member.getPassword());
+      stmt.setString(4, member.getPhoto());
+      stmt.setString(5, member.getTel());
+      stmt.setInt(6, member.getNo());
 
-			stmt.setInt(1, no);
+      if (stmt.executeUpdate() == 0) {
+        throw new Exception("회원 데이터 변경 실패!");
+      }
+    }
+  }
 
-			if (stmt.executeUpdate() == 0) {
-				throw new Exception("회원 데이터 삭제 실패!");
-			}
-		}
-	}
+  @Override
+  public void delete(int no) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "delete from pms_member where member_no=?")) {
+
+      stmt.setInt(1, no);
+
+      if (stmt.executeUpdate() == 0) {
+        throw new Exception("회원 데이터 삭제 실패!");
+      }
+    }
+  }
 }
 
 
