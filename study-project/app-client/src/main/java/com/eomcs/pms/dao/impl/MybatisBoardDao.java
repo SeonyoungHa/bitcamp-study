dea0ae9943da6ab37be5a1f54dd6e12c49243df1
SeonyoungHa <syh5004@naver.com>
2021-10-21 10:50:41 +0900

    .

--- study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MybatisBoardDao.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/dao/impl/MybatisBoardDao.java
@@ -39,98 +39,13 @@ public class MybatisBoardDao implements BoardDao {
   @Override
   public List<Board> findByKeyword(String keyword) throws Exception {
     return null;
-    //    try (PreparedStatement stmt = con.prepareStatement(
-    //        "select "
-    //            + " b.board_no,"
-    //            + " b.title,"
-    //            + " b.created_dt,"
-    //            + " b.view_cnt,"
-    //            + " m.member_no,"
-    //            + " m.name,"
-    //            + " m.email"
-    //            + " from "
-    //            + " pms_board b"
-    //            + " inner join pms_member m on b.member_no=m.member_no"
-    //            + " where b.title like(concat('%',?,'%'))"
-    //            + " or b.content like (concat('%',?,'%'))"
-    //            + " or m.name like (concat('%',?,'%'))"
-    //            + " order by b.board_no desc")) {
-    //
-    //      stmt.setString(1, keyword);
-    //      stmt.setString(2, keyword);
-    //      stmt.setString(3, keyword);
-    //
-    //      try (ResultSet rs = stmt.executeQuery()) {
-    //
-    //        ArrayList<Board> list = new ArrayList<>();
-    //
-    //        while (rs.next()) {
-    //          Board board = new Board();
-    //          board.setNo(rs.getInt("board_no"));
-    //          board.setTitle(rs.getString("title"));
-    //          board.setRegisteredDate(rs.getDate("created_dt"));
-    //          board.setViewCount(rs.getInt("view_cnt"));
-    //
-    //          Member member = new Member();
-    //          member.setNo(rs.getInt("member_no"));
-    //          member.setName(rs.getString("name"));
-    //          member.setEmail(rs.getString("email"));
-    //
-    //          board.setWriter(member);
-    //
-    //          list.add(board);
-    //        }
-    //
-    //        return list;
-    //      }
-    //    }
+    return sqlSession.selectList("BoardMapper.findByKeyword", keyword);
   }
 
   @Override
   public Board findByNo(int no) throws Exception {
     return null;
-    //    try (PreparedStatement stmt = con.prepareStatement(
-    //        "select "
-    //            + " b.board_no,"
-    //            + " b.title,"
-    //            + " b.content,"
-    //            + " b.created_dt,"
-    //            + " b.view_cnt,"
-    //            + " m.member_no,"
-    //            + " m.name,"
-    //            + " m.email"
-    //            + " from "
-    //            + " pms_board b"
-    //            + " inner join pms_member m on b.member_no=m.member_no"
-    //            + " where board_no=" + no);
-    //        ResultSet rs = stmt.executeQuery()) {
-    //
-    //      if (rs.next()) {
-    //        Board board = new Board();
-    //        board.setNo(rs.getInt("board_no"));
-    //        board.setTitle(rs.getString("title"));
-    //        board.setContent(rs.getString("content"));
-    //        board.setRegisteredDate(rs.getDate("created_dt"));
-    //        board.setViewCount(rs.getInt("view_cnt"));
-    //
-    //        Member member = new Member();
-    //        member.setNo(rs.getInt("member_no"));
-    //        member.setName(rs.getString("name"));
-    //        member.setEmail(rs.getString("email"));
-    //
-    //        board.setWriter(member);
-    //
-    //        // 조회수 증가하기
-    //        try (PreparedStatement stmt2 = con.prepareStatement(
-    //            "update pms_board set view_cnt=view_cnt + 1 where board_no=" + no)) {
-    //          stmt2.executeUpdate();
-    //        }
-    //
-    //        return board;
-    //      }
-    //
-    //      return null;
-    //    }
+    return sqlSession.selectOne("BoardMapper.findByNo", no);
   }
 
   @Override
