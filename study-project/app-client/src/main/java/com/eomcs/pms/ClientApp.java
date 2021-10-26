75379d6374112a42697702131ff9a2a08925e1ee
SeonyoungHa <syh5004@naver.com>
2021-10-26 20:26:00 +0900

    Update Setting

--- study-project/app-client/src/main/java/com/eomcs/pms/ClientApp.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -3,15 +3,12 @@ package com.eomcs.pms;
 import static com.eomcs.menu.Menu.ACCESS_ADMIN;
 import static com.eomcs.menu.Menu.ACCESS_GENERAL;
 import static com.eomcs.menu.Menu.ACCESS_LOGOUT;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
 import com.eomcs.context.ApplicationContextListener;
 import com.eomcs.menu.Menu;
 import com.eomcs.menu.MenuFilter;
@@ -54,205 +51,205 @@ import com.eomcs.util.Prompt;
 
 public class ClientApp {
 
-	SqlSession sqlSession;
+  SqlSession sqlSession;
 
-	RequestAgent requestAgent;
+  RequestAgent requestAgent;
 
-	HashMap<String,Command> commandMap = new HashMap<>();
+  HashMap<String,Command> commandMap = new HashMap<>();
 
-	class MenuItem extends Menu {
-		String menuId;
+  class MenuItem extends Menu {
+    String menuId;
 
-		public MenuItem(String title, String menuId) {
-			super(title);
-			this.menuId = menuId;
-		}
+    public MenuItem(String title, String menuId) {
+      super(title);
+      this.menuId = menuId;
+    }
 
-		public MenuItem(String title, int accessScope, String menuId) {
-			super(title, accessScope);
-			this.menuId = menuId;
-		}
+    public MenuItem(String title, int accessScope, String menuId) {
+      super(title, accessScope);
+      this.menuId = menuId;
+    }
 
-		@Override
-		public void execute() {
-			Command command = commandMap.get(menuId);
-			try {
-				command.execute(new CommandRequest(commandMap));
-			} catch (Exception e) {
-				System.out.printf("%s 명령을 실행하는 중 오류 발생!\n", menuId);
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void execute() {
+      Command command = commandMap.get(menuId);
+      try {
+        command.execute(new CommandRequest(commandMap));
+      } catch (Exception e) {
+        System.out.printf("%s 명령을 실행하는 중 오류 발생!\n", menuId);
+        e.printStackTrace();
+      }
+    }
+  }
 
-	// 옵저버 관련 필드와 메서드
-	// => 옵저버(리스너) 목록
-	List<ApplicationContextListener> listeners = new ArrayList<>();
+  // 옵저버 관련 필드와 메서드
+  // => 옵저버(리스너) 목록
+  List<ApplicationContextListener> listeners = new ArrayList<>();
 
-	// => 옵저버(리스너)를 등록하는 메서드
-	public void addApplicationContextListener(ApplicationContextListener listener) {
-		this.listeners.add(listener);
-	}
+  // => 옵저버(리스너)를 등록하는 메서드
+  public void addApplicationContextListener(ApplicationContextListener listener) {
+    this.listeners.add(listener);
+  }
 
-	// => 옵저버(리스너)를 제거하는 메서드
-	public void removeApplicationContextListener(ApplicationContextListener listener) {
-		this.listeners.remove(listener);
-	}
+  // => 옵저버(리스너)를 제거하는 메서드
+  public void removeApplicationContextListener(ApplicationContextListener listener) {
+    this.listeners.remove(listener);
+  }
 
-	private void notifyOnApplicationStarted() {
-		HashMap<String,Object> params = new HashMap<>();
-		for (ApplicationContextListener listener : listeners) {
-			listener.contextInitialized(params);
-		}
-	}
+  private void notifyOnApplicationStarted() {
+    HashMap<String,Object> params = new HashMap<>();
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextInitialized(params);
+    }
+  }
 
-	private void notifyOnApplicationEnded() {
-		HashMap<String,Object> params = new HashMap<>();
-		for (ApplicationContextListener listener : listeners) {
-			listener.contextDestroyed(params);
-		}
-	}
+  private void notifyOnApplicationEnded() {
+    HashMap<String,Object> params = new HashMap<>();
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextDestroyed(params);
+    }
+  }
 
-	public ClientApp() throws Exception {
+  public ClientApp() throws Exception {
 
-		// 서버와 통신을 담당할 객체 준비
-		requestAgent = null;
+    // 서버와 통신을 담당할 객체 준비
+    requestAgent = null;
 
-		// Mybatis의 SqlSession 객체 준비
-		sqlSession = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(
-				"com/eomcs/pms/conf/mybatis-config.xml")).openSession();
+    // Mybatis의 SqlSession 객체 준비
+    sqlSession = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(
+        "com/eomcs/pms/conf/mybatis-config.xml")).openSession();
 
-		// 데이터 관리를 담당할 DAO 객체를 준비한다.
-		BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
-		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
-		ProjectDao projectDao = sqlSession.getMapper(ProjectDao.class);
-		TaskDao taskDao = new sqlSession.getMapper(TaskDao.class);
+    // 데이터 관리를 담당할 DAO 객체를 준비한다.
+    BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
+    MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+    ProjectDao projectDao = sqlSession.getMapper(ProjectDao.class);
+    TaskDao taskDao = sqlSession.getMapper(TaskDao.class);
 
-		// Command 객체 준비
-		commandMap.put("/member/add", new MemberAddHandler(memberDao));
-		commandMap.put("/member/list", new MemberListHandler(memberDao));
-		commandMap.put("/member/detail", new MemberDetailHandler(memberDao));
-		commandMap.put("/member/update", new MemberUpdateHandler(memberDao));
-		commandMap.put("/member/delete", new MemberDeleteHandler(memberDao));
+    // Command 객체 준비
+    commandMap.put("/member/add", new MemberAddHandler(memberDao, sqlSession));
+    commandMap.put("/member/list", new MemberListHandler(memberDao));
+    commandMap.put("/member/detail", new MemberDetailHandler(memberDao));
+    commandMap.put("/member/update", new MemberUpdateHandler(memberDao, sqlSession));
+    commandMap.put("/member/delete", new MemberDeleteHandler(memberDao, sqlSession));
 
-		commandMap.put("/board/add", new BoardAddHandler(boardDao));
-		commandMap.put("/board/list", new BoardListHandler(boardDao));
-		commandMap.put("/board/detail", new BoardDetailHandler(boardDao));
-		commandMap.put("/board/update", new BoardUpdateHandler(boardDao));
-		commandMap.put("/board/delete", new BoardDeleteHandler(boardDao));
-		commandMap.put("/board/search", new BoardSearchHandler(boardDao));
+    commandMap.put("/board/add", new BoardAddHandler(boardDao, sqlSession));
+    commandMap.put("/board/list", new BoardListHandler(boardDao));
+    commandMap.put("/board/detail", new BoardDetailHandler(boardDao, sqlSession));
+    commandMap.put("/board/update", new BoardUpdateHandler(boardDao, sqlSession));
+    commandMap.put("/board/delete", new BoardDeleteHandler(boardDao, sqlSession));
+    commandMap.put("/board/search", new BoardSearchHandler(boardDao));
 
-		commandMap.put("/auth/login", new AuthLoginHandler(memberDao));
-		commandMap.put("/auth/logout", new AuthLogoutHandler());
-		commandMap.put("/auth/userinfo", new AuthUserInfoHandler());
+    commandMap.put("/auth/login", new AuthLoginHandler(memberDao));
+    commandMap.put("/auth/logout", new AuthLogoutHandler());
+    commandMap.put("/auth/userinfo", new AuthUserInfoHandler());
 
-		MemberPrompt memberPrompt = new MemberPrompt(memberDao);
+    MemberPrompt memberPrompt = new MemberPrompt(memberDao);
 
-		commandMap.put("/project/add", new ProjectAddHandler(projectDao, memberPrompt));
-		commandMap.put("/project/list", new ProjectListHandler(projectDao));
-		commandMap.put("/project/detail", new ProjectDetailHandler(projectDao));
-		commandMap.put("/project/update", new ProjectUpdateHandler(projectDao, memberPrompt));
-		commandMap.put("/project/delete", new ProjectDeleteHandler(projectDao));
+    commandMap.put("/project/add", new ProjectAddHandler(projectDao, memberPrompt, sqlSession));
+    commandMap.put("/project/list", new ProjectListHandler(projectDao));
+    commandMap.put("/project/detail", new ProjectDetailHandler(projectDao));
+    commandMap.put("/project/update", new ProjectUpdateHandler(projectDao, memberPrompt, sqlSession));
+    commandMap.put("/project/delete", new ProjectDeleteHandler(projectDao, sqlSession));
 
-		ProjectPrompt projectPrompt = new ProjectPrompt(projectDao);
-		commandMap.put("/task/add", new TaskAddHandler(taskDao));
-		commandMap.put("/task/list", new TaskListHandler(projectPrompt, taskDao));
-		commandMap.put("/task/detail", new TaskDetailHandler(taskDao));
-		commandMap.put("/task/update", new TaskUpdateHandler(taskDao));
-		commandMap.put("/task/delete", new TaskDeleteHandler(taskDao));
-	}
+    ProjectPrompt projectPrompt = new ProjectPrompt(projectDao);
+    commandMap.put("/task/add", new TaskAddHandler(taskDao, sqlSession));
+    commandMap.put("/task/list", new TaskListHandler(projectPrompt, taskDao));
+    commandMap.put("/task/detail", new TaskDetailHandler(taskDao));
+    commandMap.put("/task/update", new TaskUpdateHandler(taskDao, sqlSession));
+    commandMap.put("/task/delete", new TaskDeleteHandler(taskDao, sqlSession));
+  }
 
-	// MenuGroup에서 사용할 필터를 정의한다.
-	MenuFilter menuFilter = menu -> (menu.getAccessScope() & AuthLoginHandler.getUserAccessLevel()) > 0;
-
-
-	Menu createMainMenu() {
-		MenuGroup mainMenuGroup = new MenuGroup("메인");
-		mainMenuGroup.setMenuFilter(menuFilter);
-		mainMenuGroup.setPrevMenuTitle("종료");
-
-		mainMenuGroup.add(new MenuItem("로그인", ACCESS_LOGOUT , "/auth/login"));
-		mainMenuGroup.add(new MenuItem("내정보", ACCESS_GENERAL, "/auth/userinfo"));
-		mainMenuGroup.add(new MenuItem("로그아웃", ACCESS_GENERAL, "/auth/logout"));
-
-		mainMenuGroup.add(createBoardMenu());
-		mainMenuGroup.add(createMemberMenu());
-		mainMenuGroup.add(createProjectMenu());
-		mainMenuGroup.add(createTaskMenu());
-		mainMenuGroup.add(createAdminMenu());
-
-		return mainMenuGroup;
-	}
-
-	private Menu createBoardMenu() {
-		MenuGroup boardMenu = new MenuGroup("게시판");
-		boardMenu.setMenuFilter(menuFilter);
-		boardMenu.add(new MenuItem("등록", ACCESS_GENERAL, "/board/add"));
-		boardMenu.add(new MenuItem("목록", "/board/list"));
-		boardMenu.add(new MenuItem("상세보기", "/board/detail"));
-		boardMenu.add(new MenuItem("검색", "/board/search"));
-		return boardMenu;
-	}
-
-	private Menu createMemberMenu() {
-		MenuGroup memberMenu = new MenuGroup("회원");
-		memberMenu.setMenuFilter(menuFilter);
-		memberMenu.add(new MenuItem("등록", ACCESS_GENERAL, "/member/add"));
-		memberMenu.add(new MenuItem("목록", "/member/list"));
-		memberMenu.add(new MenuItem("상세보기", "/member/detail"));
-		return memberMenu;
-	}
-
-	private Menu createProjectMenu() {
-		MenuGroup projectMenu = new MenuGroup("프로젝트");
-		projectMenu.setMenuFilter(menuFilter);
-		projectMenu.add(new MenuItem("등록", ACCESS_GENERAL, "/project/add"));
-		projectMenu.add(new MenuItem("목록", "/project/list"));
-		projectMenu.add(new MenuItem("상세보기", "/project/detail"));
-		return projectMenu;
-	}
-
-	private Menu createTaskMenu() {
-		MenuGroup taskMenu = new MenuGroup("작업");
-		taskMenu.setMenuFilter(menuFilter);
-		taskMenu.add(new MenuItem("목록", "/task/list"));
-		return taskMenu;
-	}
-
-	private Menu createAdminMenu() {
-		MenuGroup adminMenu = new MenuGroup("관리자", ACCESS_ADMIN);
-		adminMenu.setMenuFilter(menuFilter);
-		adminMenu.add(new MenuItem("회원 등록", "/member/add"));
-		adminMenu.add(new MenuItem("프로젝트 등록", "/project/add"));
-		adminMenu.add(new MenuItem("작업 등록", "/task/add"));
-		adminMenu.add(new MenuItem("게시글 등록", "/board/add"));
-		return adminMenu;
-	}
+  // MenuGroup에서 사용할 필터를 정의한다.
+  MenuFilter menuFilter = menu -> (menu.getAccessScope() & AuthLoginHandler.getUserAccessLevel()) > 0;
 
 
-	void service() throws Exception {
+  Menu createMainMenu() {
+    MenuGroup mainMenuGroup = new MenuGroup("메인");
+    mainMenuGroup.setMenuFilter(menuFilter);
+    mainMenuGroup.setPrevMenuTitle("종료");
 
-		notifyOnApplicationStarted();
+    mainMenuGroup.add(new MenuItem("로그인", ACCESS_LOGOUT , "/auth/login"));
+    mainMenuGroup.add(new MenuItem("내정보", ACCESS_GENERAL, "/auth/userinfo"));
+    mainMenuGroup.add(new MenuItem("로그아웃", ACCESS_GENERAL, "/auth/logout"));
 
-		createMainMenu().execute();
+    mainMenuGroup.add(createBoardMenu());
+    mainMenuGroup.add(createMemberMenu());
+    mainMenuGroup.add(createProjectMenu());
+    mainMenuGroup.add(createTaskMenu());
+    mainMenuGroup.add(createAdminMenu());
 
-		Prompt.close();
+    return mainMenuGroup;
+  }
 
-		notifyOnApplicationEnded();
+  private Menu createBoardMenu() {
+    MenuGroup boardMenu = new MenuGroup("게시판");
+    boardMenu.setMenuFilter(menuFilter);
+    boardMenu.add(new MenuItem("등록", ACCESS_GENERAL, "/board/add"));
+    boardMenu.add(new MenuItem("목록", "/board/list"));
+    boardMenu.add(new MenuItem("상세보기", "/board/detail"));
+    boardMenu.add(new MenuItem("검색", "/board/search"));
+    return boardMenu;
+  }
 
-		// SqlSession 객체의 자원을 해제한다.
-		sqlSession.close();
-	}
+  private Menu createMemberMenu() {
+    MenuGroup memberMenu = new MenuGroup("회원");
+    memberMenu.setMenuFilter(menuFilter);
+    memberMenu.add(new MenuItem("등록", ACCESS_GENERAL, "/member/add"));
+    memberMenu.add(new MenuItem("목록", "/member/list"));
+    memberMenu.add(new MenuItem("상세보기", "/member/detail"));
+    return memberMenu;
+  }
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("[PMS 클라이언트]");
+  private Menu createProjectMenu() {
+    MenuGroup projectMenu = new MenuGroup("프로젝트");
+    projectMenu.setMenuFilter(menuFilter);
+    projectMenu.add(new MenuItem("등록", ACCESS_GENERAL, "/project/add"));
+    projectMenu.add(new MenuItem("목록", "/project/list"));
+    projectMenu.add(new MenuItem("상세보기", "/project/detail"));
+    return projectMenu;
+  }
 
-		ClientApp app = new ClientApp(); 
-		app.addApplicationContextListener(new AppInitListener());
-		app.service();
+  private Menu createTaskMenu() {
+    MenuGroup taskMenu = new MenuGroup("작업");
+    taskMenu.setMenuFilter(menuFilter);
+    taskMenu.add(new MenuItem("목록", "/task/list"));
+    return taskMenu;
+  }
 
-		Prompt.close();
-	}
+  private Menu createAdminMenu() {
+    MenuGroup adminMenu = new MenuGroup("관리자", ACCESS_ADMIN);
+    adminMenu.setMenuFilter(menuFilter);
+    adminMenu.add(new MenuItem("회원 등록", "/member/add"));
+    adminMenu.add(new MenuItem("프로젝트 등록", "/project/add"));
+    adminMenu.add(new MenuItem("작업 등록", "/task/add"));
+    adminMenu.add(new MenuItem("게시글 등록", "/board/add"));
+    return adminMenu;
+  }
+
+
+  void service() throws Exception {
+
+    notifyOnApplicationStarted();
+
+    createMainMenu().execute();
+
+    Prompt.close();
+
+    notifyOnApplicationEnded();
+
+    // SqlSession 객체의 자원을 해제한다.
+    sqlSession.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("[PMS 클라이언트]");
+
+    ClientApp app = new ClientApp(); 
+    app.addApplicationContextListener(new AppInitListener());
+    app.service();
+
+    Prompt.close();
+  }
 }
 
 
--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/HelloServlet.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/HelloServlet.java
@@ -2,44 +2,41 @@ package com.eomcs.pms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/hi")
 public class HelloServlet implements Servlet {
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+  }
 
-	}
+  @Override
+  public ServletConfig getServletConfig() {
+    return null;
+  }
 
-	@Override
-	public ServletConfig getServletConfig() {
-		return null;
-	}
+  @Override
+  public void service(ServletRequest req, ServletResponse res)
+      throws ServletException, IOException {
+    res.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+    out.println("안녕!");
+    out.println("Hello!");
+  }
 
-	@Override
-	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		throws SecurityException, IOException {
-			res.setContentType("tect/plain;charset=UTF-8");
-			PrintWriter out = res.getWriter();
-			out.print("Hello!")
-			out.print("Hello! 안녕!");
-		}
-	}
+  @Override
+  public String getServletInfo() {
+    return null;
+  }
 
-	@Override
-	public String getServletInfo() {
-		return null;
-	}
-
-	@Override
-	public void destroy() {
-
-	}
+  @Override
+  public void destroy() {
+  }
 
 }
