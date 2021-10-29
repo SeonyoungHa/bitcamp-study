d6b250b03cc1de9ee780d3d4260e6fc96ef0e3b6
SeonyoungHa <syh5004@naver.com>
2021-10-29 16:02:33 +0900

    Update Project3 Notes

--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberUpdateController.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberUpdateController.java
@@ -1,7 +1,6 @@
 package com.eomcs.pms.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -17,7 +16,7 @@ import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
 
 @WebServlet("/member/update")
-public class MemberUpdateHandler extends HttpServlet {
+public class MemberUpdateController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	SqlSession sqlSession;
@@ -42,40 +41,22 @@ public class MemberUpdateHandler extends HttpServlet {
 			if (member == null) {
 				throw new Exception("해당 번호의 회원이 없습니다.<br>");
 
-			} else {
+			} 
 
-				member.setName(request.getParameter("name"));
-				member.setEmail(request.getParameter("email"));
-				member.setPassword(request.getParameter("password"));
-				member.setPhoto(request.getParameter("photo"));
-				member.setTel(request.getParameter("tel"));
+			member.setName(request.getParameter("name"));
+			member.setEmail(request.getParameter("email"));
+			member.setPassword(request.getParameter("password"));
+			member.setPhoto(request.getParameter("photo"));
+			member.setTel(request.getParameter("tel"));
 
-				memberDao.update(member);
-				sqlSession.commit();
-			}
-			// 리프레시(Refresh)
-			// 서버가 보낸 콘텐트를 출력한 후에 
-			// 1초가 경과하면 지정한 URL을 요청하도록
-			// 웹브라우저에게 명령한다.
+			memberDao.update(member);
+			sqlSession.commit();
+
 			response.sendRedirect("list");
 
 		} catch (Exception e) {
-			// 변경 오류가 발생했을 때만 출력하게 한다.
-			response.setContentType("text/html;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			e.printStackTrace();
-			out.println("<html>");
-			out.println("<head>");
-			out.println("  <title>회원변경</title>");
-			out.println("</head>");
-			out.println("<body>");
-			out.println("<h1>회원변경결과</h1>");
-
-			System.out.println("회원 변경 오류");    
-			out.println("<a href='list'>[목록]</a><br>");
-			out.println("</body>");
-			out.println("</html>");
-			e.printStackTrace();
+			request.setAttribute("error", e);
+			request.getRequestDispatcher("/Error.jsp").forward(request, response);
 		}
 	}
 }
