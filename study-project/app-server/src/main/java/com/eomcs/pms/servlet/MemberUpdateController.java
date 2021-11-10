8f5ad7983168080a722c39d9402c899c3ba7fd48
SeonyoungHa <syh5004@naver.com>
2021-11-10 15:27:58 +0900

    Setting 11.10

--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberUpdateController.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberUpdateController.java
@@ -1,60 +1,72 @@
 package com.eomcs.pms.servlet;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
+import java.util.UUID;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
 import org.apache.ibatis.session.SqlSession;
+
 import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
 
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10)
 @WebServlet("/member/update")
 public class MemberUpdateController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  SqlSession sqlSession;
-  MemberDao memberDao;
+	SqlSession sqlSession;
+	MemberDao memberDao;
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
-    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-  }
+	@Override
+	public void init() {
+		ServletContext 웹애플리케이션공용저장소 = getServletContext();
+		sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
+		memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
+	}
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    try {
-      int no = Integer.parseInt(request.getParameter("no"));
+		try {
+			int no = Integer.parseInt(request.getParameter("no"));
 
-      Member member = memberDao.findByNo(no);
+			Member member = memberDao.findByNo(no);
 
-      if (member == null) {
-        throw new Exception("해당 번호의 회원이 없습니다.");
-      } 
+			if (member == null) {
+				throw new Exception("해당 번호의 회원이 없습니다.");
+			} 
 
-      member.setName(request.getParameter("name"));
-      member.setEmail(request.getParameter("email"));
-      member.setPassword(request.getParameter("password"));
-      member.setPhoto(request.getParameter("photo"));
-      member.setTel(request.getParameter("tel"));
+			member.setName(request.getParameter("name"));
+			member.setEmail(request.getParameter("email"));
+			member.setPassword(request.getParameter("password"));
+			member.setTel(request.getParameter("tel"));
 
-      memberDao.update(member);
-      sqlSession.commit();
+			Part photoPart = request.getPart("photo");
+			if (photoPart.getSize() > 0) {
+				String filename = UUID.randomUUID().toString();
+				photoPart.write(getServletContext().getRealPath("/upload/member") + "/" + filename);
+				member.setPhoto(filename);
+			} 
 
-      response.sendRedirect("list");
+			memberDao.update(member);
+			sqlSession.commit();
 
-    } catch (Exception e) {
-      request.setAttribute("error", e);
-      request.getRequestDispatcher("/Error.jsp").forward(request, response);
-    }
-  }
+			response.sendRedirect("list");
+
+		} catch (Exception e) {
+			request.setAttribute("error", e);
+			request.getRequestDispatcher("/Error.jsp").forward(request, response);
+		}
+	}
 }
 
 
