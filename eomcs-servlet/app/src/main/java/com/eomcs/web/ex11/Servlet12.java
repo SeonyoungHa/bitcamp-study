d30cd87d2000673bb9612fc5d7af82146dc46d3b
SeonyoungHa <syh5004@naver.com>
2021-11-12 16:36:57 +0900

    Setting 11.12

--- eomcs-servlet/app/src/main/java/com/eomcs/web/ex11/Servlet12.java
+++ eomcs-servlet/app/src/main/java/com/eomcs/web/ex11/Servlet12.java
@@ -3,6 +3,7 @@ package com.eomcs.web.ex11;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,25 +15,27 @@ import javax.servlet.http.HttpSession;
 @SuppressWarnings("serial")
 public class Servlet12 extends HttpServlet {
 
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    // 세션을 준비한다.
-    HttpSession session = request.getSession();
+		request.setCharacterEncoding("UTF-8");
 
-    // 클라이언트가 보낸 데이터를 세션에 보관한다.
-    session.setAttribute("name", request.getParameter("name"));
+		// 세션을 준비한다.
+		HttpSession session = request.getSession();
 
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<html><head><title>페이지2</title></head><body>");
-    out.println("<form action='s13' method='post'>");
-    out.println("나이: <input type='text' name='age'><br>");
-    out.println("<button>다음</button>");
-    out.println("</form>");
-    out.println("</body></html>");
-  }
+		// 클라이언트가 보낸 데이터를 세션에 보관한다.
+		session.setAttribute("name", request.getParameter("name"));
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>페이지2</title></head><body>");
+		out.println("<form action='s13' method='post'>");
+		out.println("나이: <input type='text' name='age'><br>");
+		out.println("<button>다음</button>");
+		out.println("</form>");
+		out.println("</body></html>");
+	}
 }
 
 
