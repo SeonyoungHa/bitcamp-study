3380eae801c8e7293f146634ec3ed730112d0341
SeonyoungHa <syh5004@naver.com>
2021-11-12 12:24:22 +0900

    Setting 11.12

--- eomcs-servlet/app/src/main/java/com/eomcs/web/ex05/MyHttpServlet.java
+++ eomcs-servlet/app/src/main/java/com/eomcs/web/ex05/MyHttpServlet.java
@@ -1,6 +1,7 @@
 package com.eomcs.web.ex05;
 
 import java.io.IOException;
+
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -17,28 +18,28 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public abstract class MyHttpServlet extends GenericServlet {
 
-  @Override
-  public void service(ServletRequest req, ServletResponse res)
-      throws ServletException, IOException {
-    // 서블릿 컨테이너가 이 메서드를 호출하면
+	@Override
+	public void service(ServletRequest req, ServletResponse res)
+			throws ServletException, IOException {
+		// 서블릿 컨테이너가 이 메서드를 호출하면
 
-    // => 파라미터 값을 원래의 타입으로 변환한다.
-    HttpServletRequest request = (HttpServletRequest) req;
-    HttpServletResponse response = (HttpServletResponse) res;
+		// => 파라미터 값을 원래의 타입으로 변환한다.
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
 
-    // => 오버로딩한 service()를 호출한다.
-    this.service(request, response);
-  }
+		// => 오버로딩한 service()를 호출한다.
+		this.service(request, response);
+	}
 
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    // 웹브라우저가 요청하면
-    // => 서블릿 컨테이너가 service(ServletRequest,ServletResponse)를 호출한다.
-    // => service(ServletRequest,ServletResponse)는
-    // 다시 오버로딩한 이 메서드를 호출한다.
-    // 현재 아무런 일을 하지 않는다.
-    // 이 클래스를 상속 받는 서브 클래스 쪽에서 이 메서드를 오버라이딩 하면 된다.
-  }
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 웹브라우저가 요청하면
+		// => 서블릿 컨테이너가 service(ServletRequest,ServletResponse)를 호출한다.
+		// => service(ServletRequest,ServletResponse)는
+		// 다시 오버로딩한 이 메서드를 호출한다.
+		// 현재 이 메서드는 아무런 일을 하지 않는다.
+		// 이 클래스를 상속 받는 서브 클래스 쪽에서 이 메서드를 오버라이딩 하면 된다.
+	}
 
 }
 
--- eomcs-servlet/app/src/main/java/com/eomcs/web/ex06/Servlet01.java
+++ eomcs-servlet/app/src/main/java/com/eomcs/web/ex06/Servlet01.java
@@ -2,6 +2,7 @@
 package com.eomcs.web.ex06;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,21 +17,21 @@ import javax.servlet.http.HttpServletResponse;
 //    웹 애플리케이션을 시작시킬 때 미리 서블릿 객체를 준비한다.
 //    예) DB 연결, 소켓 연결, 필요한 환경 변수 로딩, 스프링 IoC 컨테이너 준비 등
 //
-//@WebServlet(value = "/ex06/s1", loadOnStartup = 1)
+//@WebServlet(value ="/ex06/s1", loadOnStartup=1)
 @SuppressWarnings("serial")
 public class Servlet01 extends HttpServlet {
 
-  @Override
-  public void init() throws ServletException {
-    // 클라이언트가 서블릿의 실행을 요청할 때 서블릿 객체가 없으면 생성된다.
-    // 서블릿 객체가 생성될 때 이 메서드가 호출된다.
-    System.out.println("/ex06/s1 => init()");
-  }
+	@Override
+	public void init() throws ServletException {
+		// 클라이언트가 서블릿의 실행을 요청할 때 서블릿 객체가 없으면 생성된다.
+		// 서블릿 객체가 생성될 때 이 메서드가 호출된다.
+		System.out.println("/ex06/s1 => init()");
+	}
 
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    System.out.println("/ex06/s1 => service()");
-  }
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("/ex06/s1 => service()");
+	}
 }
 
