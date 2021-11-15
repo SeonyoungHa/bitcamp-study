98927e752350d842aa7a25c175cd81b60189b9b2
SeonyoungHa <syh5004@naver.com>
2021-11-15 13:36:05 +0900

    Setting study-project11/15

--- study-project/app-server/src/main/java/com/eomcs/pms/web/MemberDeleteController.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/web/MemberDeleteController.java
@@ -1,57 +1,26 @@
 package com.eomcs.pms.web;
 
-import java.io.IOException;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.ibatis.session.SqlSession;
-import com.eomcs.pms.dao.MemberDao;
-import com.eomcs.pms.domain.Member;
 
-@WebServlet("/member/delete")
-public class MemberDeleteController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+import com.eomcs.pms.service.MemberService;
 
-  SqlSession sqlSession;
-  MemberDao memberDao;
+public class MemberDeleteController implements Controller {
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
-    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-  }
+	MemberService memberService;
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+	public MemberDeleteController(MemberService memberService) {
+		this.memberService = memberService;
+	}
 
-    try {
-      int no = Integer.parseInt(request.getParameter("no"));
-      Member member = memberDao.findByNo(no);
-      if (member == null) {
-        throw new Exception("해당 번호의 회원이 없습니다.");
-      }
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-      memberDao.delete(no);
-      sqlSession.commit();
+		int no = Integer.parseInt(request.getParameter("no"));
+		if (memberService.delete(no) == 0) {
+			throw new Exception("해당 번호의 회원이 없습니다.");
 
-      response.sendRedirect("list");
-
-    } catch (Exception e) {
-      request.setAttribute("error", e);
-      request.getRequestDispatcher("/Error.jsp").forward(request, response);
-    }
-  }
+		}
+		return "redirect:list";
+	}
 }
-
-
-
-
-
-
-
--- study-project/app-server/src/main/java/com/eomcs/pms/web/MemberUpdateController.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/web/MemberUpdateController.java
@@ -1,100 +1,30 @@
 package com.eomcs.pms.web;
 
-import java.io.IOException;
-import java.util.UUID;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
-import org.apache.ibatis.session.SqlSession;
-import com.eomcs.pms.dao.MemberDao;
+
 import com.eomcs.pms.domain.Member;
-import net.coobird.thumbnailator.ThumbnailParameter;
-import net.coobird.thumbnailator.Thumbnails;
-import net.coobird.thumbnailator.geometry.Positions;
-import net.coobird.thumbnailator.name.Rename;
+import com.eomcs.pms.service.MemberService;
 
-@MultipartConfig(maxFileSize = 1024 * 1024 * 10)
-@WebServlet("/member/update")
-public class MemberUpdateController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+public class MemberUpdateController implements Controller {
 
-  SqlSession sqlSession;
-  MemberDao memberDao;
+	MemberService memberService;
 
-  @Override
-  public void init() {
-    ServletContext 웹애플리케이션공용저장소 = getServletContext();
-    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
-    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-  }
+	public MemberUpdateController(MemberService memberService) {
+		this.memberService = memberService;
+	}
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-    try {
-      int no = Integer.parseInt(request.getParameter("no"));
+		Member member = new Member();
+		member.setNo(Integer.parseInt(request.getParameter("no")));
+		member.setName(request.getParameter("name"));
+		member.setEmail(request.getParameter("email"));
+		member.setPassword(request.getParameter("password"));
+		member.setTel(request.getParameter("tel"));
 
-      Member member = memberDao.findByNo(no);
-
-      if (member == null) {
-        throw new Exception("해당 번호의 회원이 없습니다.");
-      } 
-
-      member.setName(request.getParameter("name"));
-      member.setEmail(request.getParameter("email"));
-      member.setPassword(request.getParameter("password"));
-      member.setTel(request.getParameter("tel"));
-
-      Part photoPart = request.getPart("photo");
-      if (photoPart.getSize() > 0) {
-        String filename = UUID.randomUUID().toString();
-        photoPart.write(getServletContext().getRealPath("/upload/member") + "/" + filename);
-        member.setPhoto(filename);
-
-        Thumbnails.of(getServletContext().getRealPath("/upload/member") + "/" + filename)
-        .size(20, 20)
-        .outputFormat("jpg")
-        .crop(Positions.CENTER)
-        .toFiles(new Rename() {
-          @Override
-          public String apply(String name, ThumbnailParameter param) {
-            return name + "_20x20";
-          }
-        });
-
-        Thumbnails.of(getServletContext().getRealPath("/upload/member") + "/" + filename)
-        .size(100, 100)
-        .outputFormat("jpg")
-        .crop(Positions.CENTER)
-        .toFiles(new Rename() {
-          @Override
-          public String apply(String name, ThumbnailParameter param) {
-            return name + "_100x100";
-          }
-        });
-      }
-
-      memberDao.update(member);
-      sqlSession.commit();
-
-      response.sendRedirect("list");
-
-    } catch (Exception e) {
-      request.setAttribute("error", e);
-      request.getRequestDispatcher("/Error.jsp").forward(request, response);
-    }
-  }
+		memberService.update(member);
+		return "redirect:list";
+	}
 }
-
-
-
-
-
-
-
