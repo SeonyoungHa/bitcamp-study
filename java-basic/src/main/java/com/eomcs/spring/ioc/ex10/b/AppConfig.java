e9e2f5487c535e472fb639b5a03975a4d05814fa
SeonyoungHa <syh5004@naver.com>
2021-11-17 15:06:12 +0900

    Setting 11/17

--- java-basic/src/main/java/com/eomcs/spring/ioc/ex10/b/AppConfig.java
+++ java-basic/src/main/java/com/eomcs/spring/ioc/ex10/b/AppConfig.java
@@ -2,9 +2,10 @@ package com.eomcs.spring.ioc.ex10.b;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import com.eomcs.spring.ioc.ex10.Car;
 
-@Configuration
+//@Configuration
 // AppConfig 클래스가 스프링 설정 정보를 갖고 있는 클래스임을 선포한다!
 // 그러면 AnnotationConfigApplicationContext 에서
 // 이 클래스를 찾아 적절한 작업을 수행할 것이다.
@@ -24,26 +25,27 @@ import com.eomcs.spring.ioc.ex10.Car;
 // Java config 클래스임을 표시할 때 붙이는 애노테이션이
 // 바로 @Configuration 이다.
 //
+@Configuration
 public class AppConfig {
-  @Bean
-  public Car car2() {
-    Car c = new Car(null);
-    c.setMaker("비트자동차");
-    c.setModel("티코");
-    c.setCc(890);
-    c.setAuto(true);
-    return c;
-  }
+	@Bean
+	public Car car2() {
+		Car c = new Car(null);
+		c.setMaker("비트자동차");
+		c.setModel("티코");
+		c.setCc(890);
+		c.setAuto(true);
+		return c;
+	}
 
-  @Bean
-  public Car car3() {
-    Car c = new Car(null);
-    c.setMaker("비트자동차");
-    c.setModel("티코");
-    c.setCc(890);
-    c.setAuto(true);
-    return c;
-  }
+	@Bean
+	public Car car3() {
+		Car c = new Car(null);
+		c.setMaker("비트자동차");
+		c.setModel("티코");
+		c.setCc(890);
+		c.setAuto(true);
+		return c;
+	}
 
 }
 
