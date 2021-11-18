f5e90cd3fc1000e0ec6364dedf54a09f23e666a1
SeonyoungHa <syh5004@naver.com>
2021-11-18 14:41:34 +0900

    Setting log4i

--- java-basic/src/main/java/com/eomcs/log4j/Exam01.java
+++ java-basic/src/main/java/com/eomcs/log4j/Exam01.java
@@ -17,28 +17,28 @@ import org.apache.logging.log4j.Logger;
 //    - 라이브러리 정보를 build.gradle 에 포함시킨다.
 //    - '$ gradle eclipse'를 실행하여 라이브러리를 가져오고, 이클립스 설정 파일을 갱신한다. 
 // 2) log4j 설정 파일을 준비 
-//    - CLASSPATH 루트 경로에 log4j2.xml 파일 작성
+//    - CLASSPATH 루트 경로에 log4j2.xml 파일 작성 
 //    - 문서를 참조하여 설정한다.
 // 
 public class Exam01 {
 
-  // 기록을 남길 때 사용할 도구 준비
-  // - 클래스 당 한 개의 도구만 있어도 된다.
-  private static final Logger logger = LogManager.getLogger(Exam01.class); 
+	// 기록을 남길 때 사용할 도구 준비
+	// - 클래스 당 한 개의 도구만 있어도 된다.
+	private static final Logger logger = LogManager.getLogger(Exam01.class); 
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    // 로깅 도구 없이 기록을 남기는 방법
-    System.out.println("고전적인 방법으로 기록을 남기기!");
+		// 로깅 도구 없이 기록을 남기는 방법
+		System.out.println("고전적인 방법으로 기록을 남기기!");
 
-    // 로깅 도구를 사용하여 기록을 남기는 방법
-    logger.fatal("내용 ==> FATAL");
-    logger.error("내용 ==> ERROR");
-    logger.warn("내용 ==> WARN");
-    logger.info("내용 ==> INFO");
-    logger.debug("내용 ==> DEBUG");
-    logger.trace("내용 ==> TRACE");
-  }
+		// 로깅 도구를 사용하여 기록을 남기는 방법
+		logger.fatal("내용 ==> FATAL");
+		logger.error("내용 ==> ERROR");
+		logger.warn("내용 ==> WARN");
+		logger.info("내용 ==> INFO");
+		logger.debug("내용 ==> DEBUG");
+		logger.trace("내용 ==> TRACE");
+	}
 }
 
 
